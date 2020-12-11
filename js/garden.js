57267ae50990418e9dac8c219d11033e139be980
Eliza Stribling <elizastribling@gmail.com>
2020-12-11 17:30:11 +1100

    adding p5

--- js/garden.js
+++ js/garden.js
@@ -1,18 +1,21 @@
-let gridSpacing = 40
 
-let W = window.innerWidth
-let H = window.innerHeight
+function setup() {
+    createCanvas(windowWidth,windowHeight);
+}
+
+function draw() {
 
 // create a grid across the width and height of the browser
 // starting at 0, until y/x is across the whole width/height, add x/y
-for (let y=0; y < H - (gridSpacing*2); y += gridSpacing) {
-    for (let x=0; x < W - (gridSpacing*2); x += gridSpacing) {
+for (let y=0; y < height; y += 15) {
+    for (let x=0; x < width; x += 15) {
         
-        let p = createP("x");
+        let p = createP(" ").size(15,15);
 
         p.position(x,y)
         p.mousePressed(change)
         p.mouseOver(change)
+        p.mouseOver(p.c)
 
     }
 }
@@ -21,14 +24,20 @@ function change() {
     
     //change the content of the p tag to be an emoji
     this.html(randomEmoji())
+    console.log(randomEmoji())
 
 }
 
 function randomEmoji() {
     
     let emojis = ["✨", "💫", "🌟", "⭐️"]
-    let output = emojis[rand]
+    let output;
+
+    let rand = floor(random(emojis.length))
+    output = emojis[rand]
 
     return output;
+
+}
 
 }
